9e2d98c237c8e4252f24df7a162573051a01129b
WEBHM2-MAIN1-MAIN 2 <taharami14@gmail.com>
2026-06-20 13:31:47 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./index.css";
+
 import Home from "./component/home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
